7e79e7fc2b6bb497032074fba0754bf30a01d3a9
DouglasCorreiaBrito <douglas.lusvardi@gmail.com>
2020-05-25 00:12:35 -0300

    adicionando end-point de monitoramento de status do servidor

--- src/main/java/br/com/jobtechIO/configuration/security/SecurityConfig.java
+++ src/main/java/br/com/jobtechIO/configuration/security/SecurityConfig.java
@@ -39,6 +39,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers(HttpMethod.POST, "/auth").permitAll()
+                .antMatchers(HttpMethod.GET,"/actuator/**").permitAll()
         .anyRequest().authenticated()
         .and()
                 .cors().disable()
